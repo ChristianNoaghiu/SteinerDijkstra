168d06ee180515efe3e2495a9b805840314fca60
EinPinguin <edvin439@web.de>
2024-04-07 19:13:54 +0200

    Alma 1 Graphenklasse

---
+++ Einführungsaufgabe/include/steinergraph.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+class SteinerGraph {
+public:
+  using NodeId = int;  // vertices are numbered 0,...,num_nodes()-1
+
+  class Neighbor {
+  public:
+        Neighbor(SteinerGraph::NodeId n, double w);
+        double edge_weight() const;
+        SteinerGraph::NodeId id() const;
+  private:
+        SteinerGraph::NodeId _id;
+        double _edge_weight;
+    };
+
+  class Node {
+  public:
+        void add_neighbor(SteinerGraph::NodeId nodeid, double weight);
+        const std::vector<Neighbor> & adjacent_nodes() const;
+  private:
+        std::vector<Neighbor> _neighbors;
+    };
+
+  enum DirType {directed, undirected};  // enum defines a type with possible values
+  SteinerGraph(NodeId num_nodes, DirType dirtype);
+  SteinerGraph(char const* filename, DirType dirtype);
+
+  void add_nodes(NodeId num_new_nodes);
+  void add_edge(NodeId tail, NodeId head, double weight = 1.0);
+
+  NodeId num_nodes() const;
+  const Node & get_node(NodeId) const;
+  void print() const;
+
+  const DirType dirtype;
+  static const NodeId invalid_node;
+  static const double infinite_weight;
+
+private:
+  std::vector<Node> _nodes;
+};
