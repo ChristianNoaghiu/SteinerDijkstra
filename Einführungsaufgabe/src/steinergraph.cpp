ce0cd35bc81ed17588a12ff40844f3489a4b2b79
EinPinguin <edvin439@web.de>
2024-04-24 09:20:01 +0200

    Graphsection und Terminalssection ausgelagert, Überprüfung der Reihenfolge hinzugefügt und Abfrage ob neuer Beginn einer Section in der aktuellen Zeile ist ausgelagert

--- Einführungsaufgabe/src/steinergraph.cpp
+++ Einführungsaufgabe/src/steinergraph.cpp
@@ -32,6 +32,133 @@ namespace
       MaximumDegreesSection = 3,
       CoordinatesSection = 4
    };
+
+   bool check_for_sections(STPSection &_section, STPSection &_last_section, const std::string &_line)
+   {
+      if (_line == stp_section_comment_line)
+      {
+         _section = CommentSection;
+         if (_section <= _last_section)
+         {
+            throw std::runtime_error("Invalid STP file: Sections are not ordered correctly.");
+         }
+         _last_section = CommentSection;
+         return true;
+      }
+      else if (_line == stp_section_graph_line)
+      {
+         _section = GraphSection;
+         if (_section <= _last_section)
+         {
+            throw std::runtime_error("Invalid STP file: Sections are not ordered correctly.");
+         }
+         return true;
+      }
+      else if (_line == stp_section_terminals_line)
+      {
+         _section = TerminalsSection;
+         if (_section <= _last_section)
+         {
+            throw std::runtime_error("Invalid STP file: Sections are not ordered correctly.");
+         }
+         return true;
+      }
+      else if (_line == stp_section_maximumdegrees_line)
+      {
+         _section = MaximumDegreesSection;
+         if (_section <= _last_section)
+         {
+            throw std::runtime_error("Invalid STP file: Sections are not ordered correctly.");
+         }
+         _last_section = MaximumDegreesSection;
+         return true;
+      }
+      else if (_line == stp_section_coordinates_line)
+      {
+         _section = CoordinatesSection;
+         if (_section <= _last_section)
+         {
+            throw std::runtime_error("Invalid STP file: Sections are not ordered correctly.");
+         }
+         _last_section = CoordinatesSection;
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   }
+
+   int graph_section(const std::string &_line, int &_num_nodes, int &_num_edges, int _head, int _tail, double &_weight, unsigned int &_edge_counter)
+   {
+      std::stringstream ss(_line);
+      std::string _keyword = "";
+      ss >> _keyword;
+      if (_keyword == stp_graph_nodes_keyword)
+      {
+         if (_num_nodes != -1)
+         {
+            throw std::runtime_error("Invalid STP file: Contains 'Nodes' keyword more than once.");
+         }
+         ss >> _num_nodes;
+         if (_num_nodes < 0)
+         {
+            throw std::runtime_error("Invalid STP file: The number of nodes cannot be negative.");
+         }
+         return 0;
+      }
+      else if (_keyword == stp_graph_edges_keyword)
+      {
+         if (_num_edges != -1)
+         {
+            throw std::runtime_error("Invalid STP file: Contains 'Nodes' keyword more than once.");
+         }
+         ss >> _num_edges;
+         if (_num_edges < 0)
+         {
+            throw std::runtime_error("Invalid STP file: The number of edges cannot be negative.");
+         }
+         return 1;
+      }
+      else if (_keyword == stp_graph_edge_keyword)
+      {
+         ss >> _head >> _tail >> _weight;
+         _edge_counter++;
+         if (_tail == _head)
+         {
+            throw std::runtime_error("Invalid STP file: Loops are not allowed.");
+         }
+         return 2;
+      }
+      return -1;
+   }
+
+   int terminals_section(const std::string &_line, int &_node, int &_num_terminals, unsigned int &_terminal_counter)
+   {
+      std::stringstream ss(_line);
+      std::string _keyword = "";
+      ss >> _keyword;
+      if (_keyword == stp_terminals_terminals_keyword)
+      {
+         if (_num_terminals != -1)
+         {
+            throw std::runtime_error("Invalid STP file: Contains 'Terminals' keyword more than once.");
+         }
+         ss >> _num_terminals;
+         if (_num_terminals < 0)
+         {
+            throw std::runtime_error("Invalid STP file: The number of terminals cannot be negative.");
+         }
+         return 0;
+      }
+      else if (_keyword == stp_terminals_terminal_keyword)
+      {
+         ss >> _node;
+         _terminal_counter++;
+         return 1;
+      }
+      return -1;
+   }
 }
 
 const SteinerGraph::NodeId SteinerGraph::invalid_node = -1;
@@ -153,6 +280,9 @@ SteinerGraph::SteinerGraph(char const *filename) // Konstruktor der Klasse   -  
 
    int num_nodes = -1, num_edges = -1, num_terminals = -1; // es fehlen noch die error-catches, falls die Zahlen zu groß werden, auch wenn das sehr unrealistisch ist...
    unsigned int edge_counter = 0, terminal_counter = 0;
+   int head = -1, tail = -1;
+   double weight = 1.0;
+   std::string keyword = "";
 
    while (std::getline(file, line))
    {
@@ -173,35 +303,21 @@ SteinerGraph::SteinerGraph(char const *filename) // Konstruktor der Klasse   -  
 
       if (current_section == NoSection) //                                                      check if section != NoSection has a new beginning of a Section, meaning, that an "END" was missing
       {
-         //                                                                                  auslagern
-         if (line == stp_section_comment_line)
+         if (!(check_for_sections(current_section, last_section, line)))
          {
-            current_section = CommentSection;
-         }
-         if (line == stp_section_graph_line)
-         {
-            current_section = GraphSection;
-         }
-         if (line == stp_section_terminals_line)
-         {
-            current_section = TerminalsSection;
-         }
-         if (line == stp_section_maximumdegrees_line)
-         {
-            current_section = MaximumDegreesSection;
-         }
-         if (line == stp_section_coordinates_line)
-         {
-            current_section = CoordinatesSection;
-         }
-         if (line != stp_empty_line)
-         {
-            throw std::runtime_error("Invalid STP file: Found non-empty line outside section.");
+            if (line != stp_empty_line)
+            {
+               throw std::runtime_error("Invalid STP file: Found non-empty line outside section.");
+            }
          }
          continue;
       }
       else
       {
+         if (check_for_sections(current_section, last_section, line))
+         {
+            throw std::runtime_error("Invalid STP file: Found beginning of new section inside of a section.");
+         }
          if (current_section == CommentSection or current_section == MaximumDegreesSection or current_section == CoordinatesSection)
          {
             if (line == stp_end_line) // Checking if the Section ends here, since nothing is done with the contents
@@ -210,50 +326,14 @@ SteinerGraph::SteinerGraph(char const *filename) // Konstruktor der Klasse   -  
             }
             continue;
          }
-         std::stringstream ss(line);
-         std::string keyword = "";
-
-         ss >> keyword;
-         if (current_section == GraphSection) // Outsource this part into a separate function(s)
+         // bool graph_section(const std::string &_line, int &_num_nodes, int &_num_edges, int _head, int _tail) wobei true für nodes und false für einzelne hinzzufügende Kanten steht
+         if (current_section == GraphSection)
          {
-            if (keyword == stp_graph_nodes_keyword)
-            {
-               if (num_nodes != -1)
-               {
-                  throw std::runtime_error("Invalid STP file: Contains 'Nodes' keyword more than once.");
-               }
-               ss >> num_nodes;      // überprüfen ob num_nodes >= 0 ist?
-               add_nodes(num_nodes); // Idee für das auslagern, dieser part bleibt in dieser Funktion, wird aber erst ganz am Ende (bei "End" zum Graphen hinzugefügt)
-            }
-            else if (keyword == stp_graph_edges_keyword)
-            {
-               if (num_nodes != -1)
-               {
-                  throw std::runtime_error("Invalid STP file: Contains 'Nodes' keyword more than once.");
-               }
-               ss >> num_edges; // überprüfen ob num_edges >= 0 ist?
-            }
-            else if (keyword == stp_graph_edge_keyword)
-            {
-               int head = -1, tail = -1; // Idee für das auslagern, dieser part bleibt in dieser Funktion und kante wird hinzugefügt, falls beide ungleich -1 sind
-               double weight = 0.0;
-               ss >> head >> tail >> weight;
-               edge_counter++;
-
-               if (tail != head)
-               {
-                  add_edge(tail, head, weight);
-               }
-               else
-               {
-                  throw std::runtime_error("Invalid STP file: Loops are not allowed.");
-               }
-            }
-            else if (keyword == stp_end_line)
+            if (line == stp_end_line)
             {
                if (num_edges == -1)
                {
-                  throw std::runtime_error("Invalid STP file: The number of edges have not been specified");
+                  throw std::runtime_error("Invalid STP file: The number of edges have not been specified.");
                }
                if (edge_counter != num_edges)
                {
@@ -261,39 +341,59 @@ SteinerGraph::SteinerGraph(char const *filename) // Konstruktor der Klasse   -  
                }
                if (num_nodes == -1)
                {
-                  throw std::runtime_error("Invalid STP file: The number of nodes have not been specified");
+                  throw std::runtime_error("Invalid STP file: The number of nodes have not been specified.");
                }
+               last_section = GraphSection;
                current_section = NoSection;
+               continue;
+            }
+            else
+            {
+               int a = graph_section(line, num_nodes, num_edges, head, tail, weight, edge_counter);
+               if (a == 0)
+               {
+                  add_nodes(num_nodes);
+               }
+               else if (a == 1 and tail != -1 and head != -1)
+               {
+                  add_edge(tail, head, weight);
+                  head = -1;
+                  tail = -1;
+               }
+               else if (a == -1)
+               {
+                  throw std::runtime_error("Invalid STP file: Invalid line in graphsection.");
+               }
+               continue;
             }
          }
          if (current_section == TerminalsSection)
          {
-            if (keyword == stp_terminals_terminals_keyword)
-            {
-               if (num_terminals != -1)
-               {
-                  throw std::runtime_error("Invalid STP file: Contains 'Terminals' keyword more than once.");
-               }
-               ss >> num_terminals;
-            }
-            else if (keyword == stp_terminals_terminal_keyword)
-            {
-               int node = -1;
-               ss >> node;
-               terminal_counter++;
-               add_terminal(node);
-            }
-            else if (keyword == stp_end_line)
+            int node = -1;
+            if (line == stp_end_line)
             {
                if (num_terminals == -1)
                {
-                  throw std::runtime_error("Invalid STP file: The number of terminals has not been specified");
+                  throw std::runtime_error("Invalid STP file: The number of terminals has not been specified.");
                }
                if (num_terminals != terminal_counter)
                {
                   throw std::runtime_error("Invalid STP file: The specified number of terminals does not match the terminalcount.");
                }
+               last_section = TerminalsSection;
                current_section == NoSection;
+            }
+            else
+            {
+               int b = terminals_section(line, node, num_terminals, terminal_counter);
+               if (b == 1)
+               {
+                  add_terminal(node);
+               }
+               else if (b == -1)
+               {
+                  throw std::runtime_error("Invalid STP file: Invalid line in terminalssection.");
+               }
             }
          }
       }
@@ -304,7 +404,7 @@ SteinerGraph::SteinerGraph(char const *filename) // Konstruktor der Klasse   -  
       throw std::runtime_error("Invalid STP file: File does not end with '" + stp_eof_line + "'.");
    }
 
-   std::stringstream ss(line); // convert line to a stringstream
+   /*std::stringstream ss(line); // convert line to a stringstream
    ss >> num;                  // for which we can use >>
    if (not ss)
    {
@@ -330,5 +430,5 @@ SteinerGraph::SteinerGraph(char const *filename) // Konstruktor der Klasse   -  
       {
          throw std::runtime_error("Invalid file format: loops not allowed.");
       }
-   }
+   } */
 }
